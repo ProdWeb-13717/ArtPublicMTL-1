e607352b7f2aaf366da807012c5d841c6ce6fe47
Jonathan Martel <Jonathmartel@gmail.com>
2016-10-31 13:21:43 -0400

    Update index.php

--- index.php
+++ index.php
@@ -22,7 +22,11 @@
    /***************************************************/
     /** Démarrage du controleur **/
     /***************************************************/
+
+	// Ajout d'un commentaire ici
 	$oCtl = new Controleur();
 	$oCtl->gerer();
+
+	// Ajout d'un commentaire ici
 
 ?>
